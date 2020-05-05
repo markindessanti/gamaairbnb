980cdda5c0c05ed4f64ea988fefd8b6024fe62da
Markin Dessanti <markindessanti@gmail.com>
2020-05-05 14:10:43 -0300

    Header, informações e lista de acomodações

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,89 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
+// Criação do objeto XMLHttpRequest.
 var request = new XMLHttpRequest()
 
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+// Abrindo uma nova conexão GET, utilizando o endpoint da API
+request.open('GET', 'https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72', true)
 
 request.onload = function () {
-	// Begin accessing JSON data here
+	// Início da manipulação dos dados recebidos
 	var data = JSON.parse(this.response)
 
-	if (request.status >= 200 && request.status < 400) {
-		data.forEach(movie => {
-			// console.log(movie.title)
+	console.log(document.styleSheets);
+	
 
-			// Create a div with a card class
+	if (request.status >= 200 && request.status < 400) {
+		data.forEach(room => {
+			// Criação da div card
 			const card = document.createElement('div')
 			card.setAttribute('class', 'card')
 
-			// Create an h1 and set the text content to the film's title
-			const h1 = document.createElement('h1')
-			h1.textContent = movie.title
+			// Criação da div da imagem
+			const imageDiv = document.createElement('div')
 
-			// Create a p and set the text content to the film's description
-			const p = document.createElement('p')
-			movie.description = movie.description.substring(0, 300) // Limit to 300 chars
-			p.textContent = `${movie.description}...` // End with an ellipses
+			// Criação e configuração do elemento img e inserção do mesmo na imageDiv
+			const imagem = document.createElement('img');
+			imagem.src = room.photo;
+			imagem.setAttribute('class', 'itemImage');
+			imageDiv.appendChild(imagem);
+			imageDiv.setAttribute('class', 'imageDiv');
 
-			// Append the cards to the container element
+			// Criação da div do nome da propriedade
+			const nomeDiv = document.createElement('div')
+
+			// Criação e configuração do elemento p e inserção do mesmo na nomeDiv
+			const nome = document.createElement('p');
+			nome.textContent = room.name;
+			nomeDiv.appendChild(nome);
+			nomeDiv.setAttribute('class', 'nomeDiv');
+
+			// Criação da div do tipo da propriedade
+			const tipoPropriedadeDiv = document.createElement('div')
+
+			// Criação e configuração do elemento p e inserção do mesmo na tipoPropriedadeDiv
+			const tipoPropriedade = document.createElement('p');
+			tipoPropriedade.textContent = room.property_type;
+			tipoPropriedadeDiv.appendChild(tipoPropriedade);
+			tipoPropriedadeDiv.setAttribute('class', 'tipoPropriedadeDiv');
+
+			// Criação da div do valor da diária
+			const priceDiv = document.createElement('div');
+			priceDiv.setAttribute('class', 'priceDiv');
+
+			// Criação e configuração do elemento p e inserção do mesmo na priceDiv
+			const p = document.createElement('p');
+			p.textContent = `R$${room.price},00`
+			p.style.fontWeight = '600';
+			p.style.fontFamily = 'Roboto';
+
+			const spamText = document.createElement('span');
+			spamText.textContent = '/noite';
+			spamText.style.fontWeight = "normal";
+			p.appendChild(spamText);
+
+			priceDiv.appendChild(p);
+
+			// Cada carda recebe suas subseções
+			card.appendChild(imageDiv)
+			card.appendChild(tipoPropriedadeDiv)
+			card.appendChild(nomeDiv)
+			card.appendChild(priceDiv)
+
+			// Inserção do card ao container
 			container.appendChild(card)
-
-			// Each card will contain an h1 and a p
-			card.appendChild(h1)
-			card.appendChild(p)
 		})
 	} else {
-		// console.log('error')
-
 		const errorMessage = document.createElement('marquee')
 		errorMessage.textContent = `Droga... Parece que não tá funcionando isso aqui...`
 		app.appendChild(errorMessage)
 	}
 }
 
-// Send request
+// Envio da requisição
 request.send()
 
 const app = document.getElementById('root');
 
-const logo = document.createElement('img');
-logo.src = 'img/logo.png';
-
 const container = document.createElement('div');
-container.setAttribute('class', 'container');
+container.setAttribute('class', 'listaAcomodacoes');
 
-app.appendChild(logo);
 app.appendChild(container);
